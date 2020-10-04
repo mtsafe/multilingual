85f54e5eafdd7ff9f58c59d26e8d21fa66eface0
Albir Tarsha <a.tarsha@yahoo.com>
2020-10-04 12:18:07 -0500

    Updates

---
+++ precache-manifest.d463836e487dda6889cd7799c531e971.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59bf29540c5e84a42009b0295ae8e0f2",
+    "url": "/multilingual/index.html"
+  },
+  {
+    "revision": "a7216ab7f14337e6306c",
+    "url": "/multilingual/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "87d19620ae864b237300",
+    "url": "/multilingual/static/js/2.06163771.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/multilingual/static/js/2.06163771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7216ab7f14337e6306c",
+    "url": "/multilingual/static/js/main.e5d72864.chunk.js"
+  },
+  {
+    "revision": "4f2834eab51d869535a4",
+    "url": "/multilingual/static/js/runtime-main.472646dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/multilingual/static/media/logo.5d5d9eef.svg"
+  }
+]);
